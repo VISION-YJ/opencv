a5dfeb70330f19044925ac7e55a00b928ca98035
Thomas <thomaskr2017@gmail.com>
2020-03-20 22:33:30 +0900

    Mat Operator => ? 실행불가

---
+++ z/test_008.cpp
@@ -0,0 +1,21 @@
+// http://www.kocw.or.kr/home/search/kemView.do?kemId=1317915&ar=relateCourse
+// Mat Operator
+// ROI (Region of Interest) : A sub-region in an image that we are interested in.
+// ROI 영상을 편집하면, 원본 영상도 영향을 받게 된다.
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat image = imread("lena.png");
+    Rect rect(0, 0, 150, 100);
+    Mat rect_roi = image(rect);
+    imshow("rectROI", rect_roi);
+
+    waitKey(0);
+    return 0;
+}
