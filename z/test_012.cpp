c91904ca0bad1b2808feaadb4f3a865927d7c9e1
Thomas <thomaskr2017@gmail.com>
2020-03-20 23:15:41 +0900

    inRange ()

---
+++ z/test_012.cpp
@@ -0,0 +1,28 @@
+// http://www.kocw.or.kr/home/search/kemView.do?kemId=1317915&ar=relateCourse
+// Mat Operator
+// inRange ()
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat image = imread("hand.png");
+    imshow("hand", image);
+    
+    cvtColor(image, image, CV_BGR2YCrCb);
+    inRange(
+        image,                  // 입력영상
+        Scalar(0, 133, 77),     // Low boundary
+        Scalar(255, 173, 127),  // Upper boundary
+        image
+    );
+        
+    imshow("inRange", image);
+    
+    waitKey(0);
+    return 0;
+}
